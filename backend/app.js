d06a1205c7497f1ed5cafd74cfeb97031bb4a095
Alex Teshome <alex_teshome@hotmail.com>
2019-07-26 17:35:39 -0400

    index.html

--- backend/app.js
+++ backend/app.js
@@ -27,12 +27,12 @@ app.use("/api/users", users);
 app.use("/api/teams", teams);
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static("client/build"));
+  app.use(express.static("../frontend/build"));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile("index.html");
   });
 }
 const PORT = process.env.PORT || 5000;
