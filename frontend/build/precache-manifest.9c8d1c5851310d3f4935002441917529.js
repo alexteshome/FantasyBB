5aa8bd53ec548ba4f44bb19b6d62f1ed33a69873
Alex Teshome <alex_teshome@hotmail.com>
2019-07-26 16:16:11 -0400

    update

---
+++ frontend/build/precache-manifest.9c8d1c5851310d3f4935002441917529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca0376869aaddb4a69c2",
+    "url": "/static/js/main.ca037686.chunk.js"
+  },
+  {
+    "revision": "75930d21c2395dac678e",
+    "url": "/static/js/1.75930d21.chunk.js"
+  },
+  {
+    "revision": "ca0376869aaddb4a69c2",
+    "url": "/static/css/main.26c96b8c.chunk.css"
+  },
+  {
+    "revision": "75930d21c2395dac678e",
+    "url": "/static/css/1.401cd206.chunk.css"
+  },
+  {
+    "revision": "28db410c0b971a8e44db735c35546a34",
+    "url": "/index.html"
+  }
+];
